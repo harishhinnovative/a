0167a18ebd04e4e4418c21ddfd55ed7c231f08f0
harishhinnovative <harishhinnovative@gmail.com>
2018-12-28 19:11:22 +0530

    home page code

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,9 +5,27 @@ class Home extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+                $this->load->helper(["common_helper"]);
+                $this->load->model(['Crud_model']);
         }
 	public function index()
 	{
-		$this->load->view('home');
+            $options = [
+              'select' => "rp.sid,rp.title as ptitle, rp.inv, rp.description, rp.price, rp.discount_per, rpi.title",
+              'table' => "r_product as rp",
+              'join' => [
+                ["r_product_images as rpi", "rp.sid = rpi.pid", "LEFT"]
+              ],
+              'where' => ["rpi.isdefault"=> 1,
+                          "rp.status"=>1,
+                          "rp.feature_product"=>1],
+                'limit' =>["8","0"],
+            ];
+            $data['featureproducts'] = $this->Crud_model->fetch_result($options);
+           // echo "<pre>";
+            //print_r($data['featureproducts']);
+            //die;
+            
+	    $this->load->view('home',$data);
 	}
 }
--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -8,3 +8,11 @@ function dd($var){
 function pr($var){
     echo "<pre>"; print_r($var);
 }
+function getActualPrice($price,$discount){
+    if($discount > 0 ){ $price = $price - ($price * $discount/100); }
+    return $price; 
+}
+function getSortDes($des){
+    $sortdes = substr($des,100)."...";
+    return $sortdes;
+}
